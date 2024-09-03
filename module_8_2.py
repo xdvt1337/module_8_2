dd966934c98b2b508c0ef6cc8d885a2060e5404d
xdvt1337 <76076932+xdvt1337@users.noreply.github.com>
2024-09-03 14:39:53 +0700

    Add files via upload

---
+++ module_8_2.py
@@ -0,0 +1,28 @@
+def personal_sum(numbers):
+    result = 0
+    incorrect_data = 0
+    for i in numbers:
+        try:
+            result += i
+        except TypeError:
+            incorrect_data += 1
+    return result, incorrect_data
+
+def calculate_average(numbers):
+    try:
+        result, incorrect = personal_sum(numbers)
+        if incorrect > 0:
+            print("Коллекция содержит нечисловые элементы.")
+        return result / len(numbers)
+    except ZeroDivisionError:
+        print("Коллекция пуста.")
+        return 0
+    except TypeError:
+        print("В numbers записан некорректный тип данных.")
+        return None
+
+
+print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
+print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
